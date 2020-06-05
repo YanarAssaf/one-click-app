7a5c3821ea842c5258fdc7a47bc5cc98edbf4354
YanarAssaf <yanar@outlook.com>
2020-06-05 23:56:42 +0300

    Update haproxy.sh

--- haproxy.sh
+++ haproxy.sh
@@ -25,71 +25,64 @@ boldwhite='\E[1;37;40m'
 
 Reset="tput sgr0"
 
-cecho ()
-{
-message=$1
-color=$2
-echo -e "$color$message" ; $Reset
-return
+cecho() {
+    message=$1
+    color=$2
+    echo -e "$color$message"
+    $Reset
+    return
 }
 clear
 
 cecho "Installing Prerequisite Packages" $boldyellow
-yum install -y -q $PRE_PACK > /dev/null
+yum install -y -q $PRE_PACK >/dev/null
 
 mkdir -p /src
 cd /src
 
 wget -q http://www.haproxy.org/download/2.1/src/haproxy-$VER.tar.gz
-if [ $? -eq 0 ]
-then
-        cecho "Downloaded Complete" $boldgreen
-        tar xzf haproxy-$VER.tar.gz && rm -f haproxy*.tar.gz;
-        cd haproxy-$VER
+if [ $? -eq 0 ]; then
+    cecho "Downloaded Complete" $boldgreen
+    tar xzf haproxy-$VER.tar.gz && rm -f haproxy*.tar.gz
+    cd haproxy-$VER
 else
-		cecho "Not Downloaded The File Check Your Internet Connection" $boldred
-		exit 1
+    cecho "Not Downloaded The File Check Your Internet Connection" $boldred
+    exit 1
 fi
 
-
 cecho "Configuring and Making HAPROXY" $boldyellow
-make TARGET=linux2628 CPU=x86_64
-if [ $? -eq 0 ]
-then
-        cecho "Successfully Finshed From Combile" $boldgreen
+make TARGET=linux-glibc CPU=x86_64
+if [ $? -eq 0 ]; then
+    cecho "Successfully Finshed From Combile" $boldgreen
 else
-		cecho "Error Not Compile Please Check Prerequisite Packages" $boldred
-		exit 1
+    cecho "Error Not Compile Please Check Prerequisite Packages" $boldred
+    exit 1
 fi
 
 cecho "Installing HAPROXY" $boldyellow
 make install
-if [ $? -eq 0 ]
-then
-        cecho "Successfully installed HAPROXY" $boldgreen
+if [ $? -eq 0 ]; then
+    cecho "Successfully installed HAPROXY" $boldgreen
 else
-		cecho "Error Not Instaleed Please Check Compile Process" $boldred
-		exit 1
+    cecho "Error Not Instaleed Please Check Compile Process" $boldred
+    exit 1
 fi
-
 
 cecho "Adding HAProxy User" $boldyellow
 id -u haproxy &>/dev/null || useradd -s /usr/sbin/nologin -r haproxy
 
-
 cecho "Creating config and Adding Daemon" $boldyellow
 
-cp /usr/local/sbin/haproxy* /usr/sbin/     # copy binaries to /usr/sbin
-cp /src/haproxy-$VER/examples/haproxy.init /etc/init.d/haproxy     # copy init script in /etc/init.d
-chmod +x /etc/init.d/haproxy     # setting permission on init script
-mkdir -p /etc/haproxy     # creating directory where the config file must reside
-cp /src/haproxy-$VER/examples/option-http_proxy.cfg /etc/haproxy/haproxy.cfg     # copy example config file
-mkdir -p /var/lib/haproxy     # create directory for stats file
-touch /var/lib/haproxy/stats     # creating stats file
-
+cp /usr/local/sbin/haproxy* /usr/sbin/                                       # copy binaries to /usr/sbin
+cp /src/haproxy-$VER/examples/haproxy.init /etc/init.d/haproxy               # copy init script in /etc/init.d
+chmod +x /etc/init.d/haproxy                                                 # setting permission on init script
+mkdir -p /etc/haproxy                                                        # creating directory where the config file must reside
+cp /src/haproxy-$VER/examples/option-http_proxy.cfg /etc/haproxy/haproxy.cfg # copy example config file
+mkdir -p /var/lib/haproxy                                                    # create directory for stats file
+touch /var/lib/haproxy/stats                                                 # creating stats file
 
 cecho "checking config and starting service" $boldyellow
-service haproxy check     # checking configuration file is valid
-chkconfig haproxy on     # setting haproxy to start with VM
+service haproxy check # checking configuration file is valid
+chkconfig haproxy on  # setting haproxy to start with VM
 
 exit 0
