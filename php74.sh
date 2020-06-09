adc10e526abf3e9704b31b2590308063a600201f
YanarAssaf <yanar@outlook.com>
2020-06-09 10:13:59 +0300

    Update php74.sh

--- php74.sh
+++ php74.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
-yum -y install http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
 
+### VARIABLES ###
+PRE_PACK="php php-fpm php-opcache php-devel php-mbstring php-mcrypt php-mysql php-mysqlnd php-phpunit-PHPUnit php-pecl-xdebug php-pecl-xhprof"
+VER=""
+
+# Setup Colours
+boldblack='\E[1;30;40m'
+boldred='\E[1;31;40m'
+boldgreen='\E[1;32;40m'
+boldyellow='\E[1;33;40m'
+boldblue='\E[1;34;40m'
+
+Reset="tput sgr0"
+
+cecho() {
+    message=$1
+    color=$2
+    echo -e "$color$message"
+    $Reset
+    return
+}
+clear
+
+yum -y -q install http://rpms.famillecollet.com/enterprise/remi-release-7.rpm >/dev/null
 $(sed -i '9s|enabled=0|enabled=1|g' /etc/yum.repos.d/remi-php74.repo)
-
-yum -y install php php-fpm php-opcache php-devel php-mbstring php-mcrypt php-mysql php-mysqlnd php-phpunit-PHPUnit php-pecl-xdebug php-pecl-xhprof -y
+cecho "Installing Prerequisite Packages..." $boldyellow
+yum install -y -q $PRE_PACK >/dev/null
 
 #Apache Setting
 $(sed -i 's|#LoadModule proxy_module modules\/mod_proxy.so|LoadModule proxy_module modules\/mod_proxy.so|g' /usr/local/apache2/conf/httpd.conf)
 $(sed -i 's|#LoadModule proxy_fcgi_module modules\/mod_proxy_fcgi.so|LoadModule proxy_fcgi_module modules\/mod_proxy_fcgi.so|g' /usr/local/apache2/conf/httpd.conf)
 $(sed -i 's|#LoadModule rewrite_module modules/mod_rewrite.so|LoadModule rewrite_module modules\/mod_rewrite.so|g' /usr/local/apache2/conf/httpd.conf)
 
-echo '
+cecho "Setup Apache setting with fpm" $boldyellow
+cecho '
     <LocationMatch "^/(.*\.php(/.*)?)$">
         ProxyPass fcgi://127.0.0.1:9000/var/www/ex/$1
     </LocationMatch>
@@ -19,7 +42,7 @@ echo '
         Require all granted
         SetHandler proxy:fcgi://127.0.0.1:9000
     </FilesMatch>
-'
+' $boldred
 
 $(sed -i 's|short_open_tag = Off|short_open_tag = On|g' /etc/php.ini)
 $(sed -i 's|expose_php = On|expose_php = Off|g' /etc/php.ini)
@@ -34,8 +57,14 @@ $(sed -i 's|;open_basedir =|open_basedir ="/var/www/"|g' /etc/php.ini)
 $(sed -i 's|;cgi.fix_pathinfo=1|cgi.fix_pathinfo=0|g' /etc/php.ini)
 $(sed -i 's|;error_log = php_errors.log|error_log = php_errors.log|g' /etc/php.ini)
 
-$(printf "<?php\nphpinfo();\n" >/var/www/ex/info.php)
-
 systemctl restart php-fpm
+
+cecho '\n
+# Optional PHP-FPM: Multiple Resource Pools \n
+groupadd www-data
+useradd -r -g www-data -s /sbin/nologin -M www-data
+mv /etc/php-fpm.d/www.conf /etc/php-fpm.d/www.conf.bk
+printf "[www]\\nuser = www-data\\ngroup = www-data\\nlisten = 127.0.0.1:9000\\nlisten.owner = apache\\nlisten.group = apache\\nphp_admin_value[disable_functions] = show_source, system, shell_exec, passthru, exec,  popen, proc_open ,  phpmail, phpinfo\\nphp_admin_flag[allow_url_fopen] = off\\nphp_admin_value[open_basedir] = /var/www/ex\npm = dynamic\\npm.max_children = 5\\npm.start_servers = 2\\npm.min_spare_servers = 1\\npm.max_spare_servers = 3\nchdir = / " > /etc/php-fpm.d/ex.conf
+' $boldgreen
 
 exit 0
