753fe942fca9028d37e3f2ea76c742f1ecbc4ed6
YanarAssaf <yanar@outlook.com>
2022-01-25 14:49:22 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,7 +2,7 @@
 
 ### VARIABLES ###
 EXT_PACK="wget vim net-tools htop mtr ntp rsync bash-completion bash-completion-extras" 
-PRE_PACK="yum-utils"
+PRE_PACK="yum-utils device-mapper-persistent-data lvm2"
 
 # Setup Colours
 boldblack='\E[1;30;40m'
@@ -25,22 +25,21 @@ cecho() {
 }
 clear
 
+setenforce 0
+sed -i --follow-symlinks 's/SELINUX=permissive/SELINUX=disabled/g' /etc/sysconfig/selinux
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+swapoff -a
+yum -y remove firewalld
+
 cecho "Installing Prerequisite Packages..." $boldyellow
 yum install -y -q epel-release >/dev/null
 yum install -y -q $EXT_PACK >/dev/null
 yum install -y -q $PRE_PACK >/dev/null
-
-
+cecho "Downloading and installing Docker..." $boldyellow
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo >/dev/null
 cecho "Installing Docker..." $boldyellow
 yum -y -q install docker-ce docker-ce-cli containerd.io >/dev/null
 systemctl daemon-reload && systemctl restart docker 
-curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose >/dev/null 2>&1
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose >/dev/null 2>&1
 chmod +x /usr/local/bin/docker-compose
-
-# Proxy
-# echo "proxy=http://51.15.80.136:3128" >> /etc/yum.conf
-# mkdir /etc/systemd/system/docker.service.d
-# touch /etc/systemd/system/docker.service.d/http-proxy.conf
-# echo "[Service]" >> /etc/systemd/system/docker.service.d/http-proxy.conf
-# echo "Environment=\"HTTP_PROXY=http://51.15.80.136:3128/\"" >> /etc/systemd/system/docker.service.d/http-proxy.conf
+cecho "Download & install has been completed\n" $boldgreen
