32d21fbb1fa19d312e07973751b281cf62e5d2cd
YanarAssaf <yanar@outlook.com>
2022-11-25 17:55:06 +0200

    Create grafana.sh

---
+++ el9/grafana.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+### VARIABLES ###
+PRE_PACK="" 
+VER=""
+
+# Setup Colours
+boldblack='\E[1;30;40m'
+boldred='\E[1;31;40m'
+boldgreen='\E[1;32;40m'
+boldyellow='\E[1;33;40m'
+boldblue='\E[1;34;40m'
+boldmagenta='\E[1;35;40m'
+boldcyan='\E[1;36;40m'
+boldwhite='\E[1;37;40m'
+
+Reset="tput sgr0"
+
+cecho() {
+    message=$1
+    color=$2
+    echo -e "$color$message"
+    $Reset
+    return
+}
+clear
+
+cat <<EOF > /etc/yum.repos.d/grafana.repo
+[grafana]
+name=grafana
+baseurl=https://rpm.grafana.com
+repo_gpgcheck=1
+enabled=1
+gpgcheck=1
+gpgkey=https://rpm.grafana.com/gpg.key
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+EOF
+
+cecho "Downloading and instaling Grafana..." $boldyellow
+yum -y -q install grafana >/dev/null
+systemctl enable grafana-server
+cecho "Download & install has been completed" $boldgreen
