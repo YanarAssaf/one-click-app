cf6da59bd831885c539880727ba398069be89364
YanarAssaf <yanar@outlook.com>
2022-11-27 15:14:55 +0200

    Create nginx.sh

---
+++ el9/nginx.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+EXT_PACK="wget vim net-tools htop mtr rsync bash-completion" 
+PRE_PACK="yum-utils"
+
+boldblack='\E[1;30;40m'
+boldred='\E[1;31;40m'
+boldgreen='\E[1;32;40m'
+boldyellow='\E[1;33;40m'
+boldblue='\E[1;34;40m'
+
+Reset="tput sgr0"
+
+cecho() {
+    message=$1
+    color=$2
+    echo -e "$color$message"
+    $Reset
+    return
+}
+clear
+
+systemctl stop firewalld ; systemctl disable firewalld
+setenforce 0
+sed -i --follow-symlinks 's/SELINUX=permissive/SELINUX=disabled/g' /etc/sysconfig/selinux
+
+cecho "Installing Prerequisite Packages..." $boldyellow
+dnf install -y -q epel-release >/dev/null
+dnf install -y -q $EXT_PACK >/dev/null
+dnf install -y -q $PRE_PACK >/dev/null
+
+$(printf "[nginx-stable]\nname=nginx stable repo\nbaseurl=http://nginx.org/packages/centos/\$releasever/\$basearch/\ngpgcheck=1\nenabled=1\ngpgkey=https://nginx.org/keys/nginx_signing.key\nmodule_hotfixes=true" >/etc/yum.repos.d/nginx.repo)
+cecho "Installing Nginx..." $boldyellow
+dnf -y install nginx >/dev/null 2>&1
+cecho "Install has been completed" $boldgreen
+
+exit 0
