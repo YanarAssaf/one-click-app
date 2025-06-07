fd04c700a2b5e730b83995b07e0e28c1222c8080
YanarAssaf <yanar@outlook.com>
2025-06-07 11:42:20 +0300

    Create docker.sh

---
+++ ubuntu22/docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+###Ubuntu22
+##Docker
+
+EXT_PACK="" 
+PRE_PACK=""
+
+boldblack='\E[1;30;40m'
+boldred='\E[1;31;40m'
+boldgreen='\E[1;32;40m'
+boldyellow='\E[1;33;40m'
+boldblue='\E[1;34;40m'
+
+Reset="tput sgr0"
+
+cecho() {
+    message=$1
+    color=$2
+    echo -e "$color$message"
+    $Reset
+    return
+}
+clear
+
+cecho "Installing Docker..." $boldyellow
+apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update -y 
+apt-cache policy docker-ce -y 
+sudo apt install docker-ce docker-compose -y 
+
+cecho "Install has been completed" $boldgreen
+
+exit 0
