5b50cc0ffcbd2d5aaaaf3ddd6b73acd123716679
YanarAssaf <yanar@outlook.com>
2026-05-25 20:53:34 +0300

    Update httpdsrc.sh

--- el10/httpdsrc.sh
+++ el10/httpdsrc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ### VARIABLES ###
-EXT_PACK="wget vim net-tools htop mtr nload tcpdump rsync bash-completion" 
+EXT_PACK="tar wget vim net-tools htop mtr nload tcpdump rsync bash-completion" 
 PRE_PACK="autoconf libtool openssl-devel pcre2-devel expat-devel"
 VER="2.4.67"
 
