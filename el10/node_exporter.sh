0fc0eec58d19ac1086a91454113eaa96660d3e03
YanarAssaf <yanar@outlook.com>
2026-05-26 11:20:43 +0300

    Create node_exporter.sh

---
+++ el10/node_exporter.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+### VARIABLES ###
+PRE_PACK="" 
+VER="1.11.1"
+
+# Setup Colours
+boldblack='\E[1;30;40m'
+boldred='\E[1;31;40m'
+boldgreen='\E[1;32;40m'
+boldyellow='\E[1;33;40m'
+boldblue='\E[1;34;40m'
+boldmagenta='\E[1;35;40m'
+boldcyan='\E[1;36;40m'
+boldwhite='\E[1;37;40m'
+
+Reset="tput sgr0"
+
+cecho() {
+    message=$1
+    color=$2
+    echo -e "$color$message"
+    $Reset
+    return
+}
+clear
+
+
+cecho "Downloading and instaling node_exporter..." $boldyellow
+
+#wget -q https://github.com/prometheus/node_exporter/releases/download/v1.4.0/node_exporter-1.11.1.linux-amd64.tar.gz >/dev/null
+wget -q https://yanarit.com/node_exporter-$VER.linux-amd64.tar.gz >/dev/null
+
+tar zxvf node_exporter-$VER.linux-amd64.tar.gz >/dev/null
+sudo useradd --no-create-home --shell /sbin/nologin node_exporter
+mv node_exporter-$VER.linux-amd64/node_exporter /usr/local/bin/
+sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
+
+
+cecho "Download & install has been completed" $boldgreen
+
+cat <<EOF > /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Prometheus Node Exporter
+Documentation=https://github.com/prometheus/node_exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+Type=simple
+User=node_exporter
+Group=node_exporter
+ExecStart=/usr/local/bin/node_exporter \
+  --collector.systemd \
+  --collector.processes
+Restart=always
+RestartSec=5
+SyslogIdentifier=node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable node_exporter.service --now
