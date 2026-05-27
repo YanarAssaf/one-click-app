0545ab25d4dfc9b797b6d4af83a78da24a3d18dc
YanarAssaf <yanar@outlook.com>
2026-05-27 17:23:27 +0300

    Update nagios.sh

--- el10/nagios.sh
+++ el10/nagios.sh
@@ -87,4 +87,13 @@ else
     exit 1
 fi
 
+cecho "commnad to install nrpe on clients" $boldgreen
+
+echo ""
+
+cecho "alerting:
+  dnf config-manager --set-enabled crb
+  dnf install nrpe nagios-plugins-all " $boldgreen
+
+
 cecho "finished" $boldgreen
