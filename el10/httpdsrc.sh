d403573837cd35bc2624f7a873ab4a829fd0ffca
YanarAssaf <yanar@outlook.com>
2026-05-27 13:53:13 +0300

    Update httpdsrc.sh

--- el10/httpdsrc.sh
+++ el10/httpdsrc.sh
@@ -28,7 +28,7 @@ clear
 
 systemctl disable --now firewalld
 setenforce 0
-sed -i --follow-symlinks 's/SELINUX=permissive/SELINUX=disabled/g' /etc/sysconfig/selinux
+sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
 
 cecho "Installing Prerequisite Packages..." $boldyellow
 dnf install -y -q epel-release >/dev/null
