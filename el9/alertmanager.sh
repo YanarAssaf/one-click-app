901bb68e4733cfb4ab5f2905a201456c25f23a75
YanarAssaf <yanar@outlook.com>
2026-05-27 14:23:10 +0300

    Update alertmanager.sh

--- el9/alertmanager.sh
+++ el9/alertmanager.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 ### VARIABLES ###
-PRE_PACK="" 
-VER=""
+PRE_PACK="epel-release"
+EXT_PACK="tar wget vim net-tools htop mtr nload tcpdump rsync bash-completion" 
+VER="0.32.1"
 
 # Setup Colours
 boldblack='\E[1;30;40m'
@@ -25,16 +26,25 @@ cecho() {
 }
 clear
 
+systemctl disable firewalld --now
+setenforce 0
+sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+
+cecho "Installing Prerequisite Packages..." $boldyellow
+dnf -y -q install $PRE_PACK >/dev/null
+dnf -y -q install $EXT_PACK >/dev/null
 
 cecho "Downloading and installing Prometheus..." $boldyellow
 
 useradd --no-create-home --shell /bin/false alertmanager
-wget https://github.com/prometheus/alertmanager/releases/download/v0.24.0/alertmanager-0.24.0.linux-amd64.tar.gz >/dev/null
-tar zxvf alertmanager-0.24.0.linux-amd64.tar.gz
+#wget https://github.com/prometheus/alertmanager/releases/download/v0.24.0/alertmanager-0.24.0.linux-amd64.tar.gz >/dev/null
+wget https://yanarit.com/alertmanager-$VER.linux-amd64.tar.gz >/dev/null
+
+tar zxvf alertmanager-$VER.linux-amd64.tar.gz
 
 mkdir -p /etc/alertmanager
-mv alertmanager-0.24.0.linux-amd64/{alertmanager,amtool} /usr/local/bin/
-mv alertmanager-0.24.0.linux-amd64/alertmanager.yml /etc/alertmanager
+mv alertmanager-$VER.linux-amd64/{alertmanager,amtool} /usr/local/bin/
+mv alertmanager-$VER.linux-amd64/alertmanager.yml /etc/alertmanager
 chown alertmanager:alertmanager /usr/local/bin/{alertmanager,amtool}
 chown -R alertmanager:alertmanager /etc/alertmanager/
 
@@ -53,14 +63,15 @@ Type=simple
 WorkingDirectory=/etc/alertmanager/
 ExecStart=/usr/local/bin/alertmanager \
     --config.file=/etc/alertmanager/alertmanager.yml
+Restart=always
+RestartSec=5
 	
 [Install]
 WantedBy=multi-user.target
 EOF
 
 systemctl daemon-reload
-systemctl restart alertmanager
-systemctl enable alertmanager
+systemctl enable --now alertmanager
 
 cat <<EOF > /etc/alertmanager/alertmanager.yml
 global:
@@ -80,8 +91,17 @@ receivers:
   - to: 'admin'
 EOF
 
-sed -i '4i \rule_files:\n  - rules.yml\n' /etc/prometheus/prometheus.yml
-sed -i '7i \alerting:\n  alertmanagers:\n  - static_configs:\n    - targets:\n      - localhost:9093\n' /etc/prometheus/prometheus.yml
+#sed -i '7i \alerting:\n  alertmanagers:\n  - static_configs:\n    - targets:\n      - localhost:9093\n' /etc/prometheus/prometheus.yml
+
+cecho "Add alerting conf setting to your prometheus conf file:" $boldgreen
+echo ""
+
+# Fix: Quote the entire multi-line string so it passes as a single argument ($1)
+cecho "alerting:
+  alertmanagers:
+  - static_configs:
+    - targets:
+      - localhost:9093" $boldgreen
     
 cecho "Example Rules" $boldgreen
-cecho "https://awesome-prometheus-alerts.grep.to/rules.html#rule-redis-1-9"
+cecho "https://samber.github.io/awesome-prometheus-alerts/rules"
