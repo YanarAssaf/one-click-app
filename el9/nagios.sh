080667dea0fafbcb9686629b585ee89f1c153a68
YanarAssaf <yanar@outlook.com>
2026-05-27 16:47:27 +0300

    Update nagios.sh

--- el9/nagios.sh
+++ el9/nagios.sh
@@ -2,6 +2,7 @@
 
 ### VARIABLES ###
 PRE_PACK="gcc glibc glibc-common perl httpd php wget gd gd-devel s-nail postfix openssl-devel"
+PRE_PACK1="make gettext automake autoconf  net-snmp net-snmp-utils perl-Net-SNMP"
 EXT_PACK="tar wget vim net-tools htop mtr nload tcpdump rsync bash-completion" 
 
 VER="4.5.12"
@@ -36,8 +37,9 @@ sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig
 
 cecho "Installing Prerequisite Packages..." $boldyellow
 dnf install -y -q epel-release >/dev/null
-dnf install -y -q $EXT_PACK >/dev/null
 dnf install -y -q $PRE_PACK >/dev/null
+dnf install -y -q $PRE_PACK1 >/dev/null
+dnf install -y -q $EXT_PACK >/dev/null
 cd /tmp
 
 cecho "Downloading and installing Nagios..." $boldyellow
@@ -70,5 +72,19 @@ htpasswd -cb "$USER_FILE" "$USER_NAME" "$USER_PASS"
 systemctl --now enable httpd.service 
 systemctl --now enable nagios.service
 
+#Nagios-pligins
+cd /tmp
+wget -O nagios-plugins.tar.gz -q https://yanarit.com/nagios-plugins-2.5.tar.gz
+tar zxf nagios-plugins.tar.gz
+cd /tmp/nagios-plugins-*
+
+./configure >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+	make >/dev/null 2>&1
+	make install >/dev/null 2>&1
+else
+    cecho "Installation Failed" $boldred
+    exit 1
+fi
 
 cecho "finished" $boldgreen
